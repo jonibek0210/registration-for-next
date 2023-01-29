43d07418156f18dbf6e04c9a74548a28c29d0bce
jony <sabinalatipova20@gmail.com>
2023-01-29 23:11:39 +0500

    private page

--- src/pages/me/index.jsx
+++ src/pages/me/index.jsx
@@ -1,9 +1,16 @@
+import PrivateFooter from "@/components/PrivateFooter";
+import PrivateHeader from "@/components/PrivateHeader";
+
 const index = () => {
    return (
-      <div className="">
-         wqfqf
-      </div>
+      <>
+         <PrivateHeader />
+         <main className="container m-auto">
+            <h1>content</h1>
+         </main>
+         <PrivateFooter />
+      </>
    );
 }
-   
+
 export default index;
